558ca9a7c56793e79591f72f55e0bff7f5eb38e5
Krish <krishagarwal1673@gmail.com>
2024-09-30 11:34:56 +0530

    Create the authentication routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,15 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { parseSigned } from 'hono/utils/cookie'
-
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings : {
-    DATABASE_URL : string
+    DATABASE_URL : string,
+    JWT_SECRET : string
   }
 }>()
+
 
 app.post('api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
@@ -28,29 +29,64 @@ app.post('api/v1/signup', async (c) => {
     return c.text("Password is missing")
   }
   try{
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data : {
         name : body.name,
         email : body.email,
         password : body.password,
       },
     })
+    const token = await sign({id : user.id}, c.env.JWT_SECRET)
     c.status(200)
-    return c.text('Sucessful Signup')
+    return c.json({ 
+      message : "Signup Succesfull",
+      token : token
+    })
   }catch(e){
-    console.log(e)
-    c.status(411)
-    return c.text("Error")
+    if (e.code === 'P2002' && e.meta && e.meta.target.includes('email')) {
+      c.status(409) // Conflict
+      return c.text('Email already exists. Please use a different email.')
+    }
+  }
+  c.status(500)
+  return c.text("Internal Server Error")
+})
+
+
+app.post('api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json()
+  if(!body || !body.email || !body.password){
+    c.status(408)
+    return c.json({message : "Incomplete Details"});
+  }
+  try{
+    const user = await prisma.user.findUnique({
+      where : {
+        email : body.email
+      },
+    })
+    if(!user){
+      c.status(404)
+      return c.json({message : "User Doesn't Exists"})
+    }
+    if(body.password != user.password){
+      c.status(401)
+      return c.json({message : "Invalid Password"})
+    }
+    c.status(200)
+    return c.json({message : "Login Succesfull"})
+  }catch(e){
+    c.status(500)
+    return c.json({message : "An error occurred"})
   }
 })
 
 
-app.post('api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
-})
-
-
 app.post('api/v1/blog', (c) => {
+  
   return c.text('Hello Hono!')
 })
 
